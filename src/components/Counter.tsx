1c9a972df1e88030316901ec2487b499676ee767
Friske2 <parnupat.j@gmail.com>
2025-02-15 09:46:41 +0700

    lint format

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,27 @@
-import useCount from '../hooks/useCount'
-function Counter(){
-  const { count, increment, decrement } = useCount()
-  return <div>
-    <div>Count : {count}</div>
-    <button className='btn--dark' onClick={()=> {
-      increment()
-    }}>+</button>
-    <button className='btn--dark' onClick={()=> {
-      decrement()
-    }}>-</button>
-  </div>
+import useCount from "../hooks/useCount";
+function Counter() {
+  const { count, increment, decrement } = useCount();
+  return (
+    <div>
+      <div>Count : {count}</div>
+      <button
+        className="btn--dark"
+        onClick={() => {
+          increment();
+        }}
+      >
+        +
+      </button>
+      <button
+        className="btn--dark"
+        onClick={() => {
+          decrement();
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
 }
 
-export default Counter
+export default Counter;
